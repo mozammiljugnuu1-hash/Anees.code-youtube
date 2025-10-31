68a59540d2fee67d0080bbd7db07bd1fe159fb6a
mozammiljugnuu1-hash <mozammiljugnuu1@gmail.com>
2025-10-31 13:04:33 +0000

    how are you

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all js code as newer version 
+
+//alert( 3 + 3)// we are using node.js, not browser
+
+console.log(3
+    +
+    3)// code readability should be high
+
+console.log("Anees")
+
+let name = "Anees Asif"
+let age = 13
+let isLoggedIn = "false"
+let state = null 
+
+//number => 2 to power of 53
+//bigInt
+//string => ""
+//boolean => true/false
+//null => standLone vlaue 
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof age) ; //number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
